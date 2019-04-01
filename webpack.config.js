b46b11a3d01cd4b4f6e0ad83688c0af5d3795588
Jen Nummerdor <jennifer.nummerdor@gmail.com>
2019-04-01 16:42:10 -0700

    Many changes have been made. CSS modules are up and working! :tada:

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,9 @@ module.exports = {
         },
       },
       {
-        test:/\.css$/,
-        use:['style-loader','css-loader']
-      }
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+    }
     ],
   },
   output: {
